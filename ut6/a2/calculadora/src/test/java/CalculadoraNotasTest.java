f37bc070b591001adc5be8bfdc717dd1a590c07e
Javier <javierreyesperdomo@gmail.com>
2026-03-19 20:27:02 +0000

    añado ut6/a2

---
+++ ut6/a2/calculadora/src/test/java/CalculadoraNotasTest.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class CalculadoraNotasTest {
+
+    @Test
+    void testMediaSimple(){
+        assertEquals(7, CalculadoraNotas.calcularMedia(new int[]{6,7,8}));
+    }
+
+}
